fe096e90eedb77ab09101fdf338a7dbf781b5d90
Liam <liam.pesser@gmail.com>
2020-11-29 15:47:47 +0100

    Intégration du template des Todos

--- src/js/data.js
+++
@@ -1,4 +0,0 @@
-export default [
-  {id: 1, content: "Tâche 1", completed: false},
-  {id: 2, content: "Tâche 2", completed: false}
-];
--- src/js/modules/Todo.js
+++
@@ -1,7 +0,0 @@
-export default class Todo {
-  constructor (todo) {
-    this.id = todo.id;
-    this.content = todo.content;
-    this.completed = todo.completed;
-  }
-}
